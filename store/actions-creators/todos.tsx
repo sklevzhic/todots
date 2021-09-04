7bc5d7d9e2f0b09f4faf1abd67d833f964dd4f48
Alexander <sklevzhic@gmail.com>
2021-09-04 23:02:46 +0300

    fix

--- store/actions-creators/todos.tsx
+++ store/actions-creators/todos.tsx
@@ -2,10 +2,13 @@ import {AddToDoType, TodoAction, ToDoActionTypes} from "../../types/todo";
 import {Dispatch} from "react";
 import axios from "axios";
 
+const URL = 'https://technicsklevzhits.herokuapp.com'
+// const URL = 'http://localhost:3004'
+
 export const fetchTodos = () => {
     return async (dispatch: Dispatch<TodoAction>) => {
         try {
-            const response = await axios.get('http://localhost:3004/todos')
+            const response = await axios.get(URL + '/todos')
             dispatch({type: ToDoActionTypes.FETCH_TODOS, payload: response.data})
         } catch (e) {
             dispatch({
@@ -18,7 +21,7 @@ export const fetchTodos = () => {
 export const addTodo = (obj: AddToDoType) => {
     return async (dispatch: Dispatch<TodoAction>) => {
         try {
-            const response = await axios.post('http://localhost:3004/todos', obj)
+            const response = await axios.post(URL + '/todos', obj)
             dispatch({type: ToDoActionTypes.ADD_TODO, payload: response.data})
         } catch (e) {
             dispatch({
@@ -31,7 +34,7 @@ export const addTodo = (obj: AddToDoType) => {
 export const deleteTodo = (id: number) => {
     return async (dispatch: Dispatch<TodoAction>) => {
         try {
-            const response = await axios.delete(`http://localhost:3004/todos/${id}`)
+            const response = await axios.delete(URL + `/todos/${id}`)
             dispatch({type: ToDoActionTypes.DELETE_TODO, payload: id})
         } catch (e) {
             dispatch({
@@ -43,12 +46,12 @@ export const deleteTodo = (id: number) => {
 export const completedTodo = (id: number, completed: boolean) => {
     return async (dispatch: Dispatch<TodoAction>) => {
         try {
-            const response = await axios.patch(`http://localhost:3004/todos/${id}`, {completed: !completed})
+            const response = await axios.patch(URL + `/todos/${id}`, {completed: !completed})
             dispatch({type: ToDoActionTypes.UPDATE_TODO, payload: response.data})
         } catch (e) {
             dispatch({
                 type: ToDoActionTypes.FETCH_TODOS_ERROR,
-                payload: 'Произошла ошибка при удалении задания'})
+                payload: 'Произошла ошибка'})
         }
     }
 }
